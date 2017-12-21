fa34e941411b4c7cc96945e456f3626916e0f9ac
Erik <erik.desjardins@gmail.com>
2017-12-21 00:44:15 -0500

    Integrated GAPI into build tree

--- webapp/dev/app.js
+++ webapp/dev/app.js
@@ -1,5 +1,9 @@
+import gapi from './api/gapi';
 import { h, render } from 'preact';
 import HeaderWrapper from './wrappers/header';
+
+global.addEventListener('blur',  () => { document.body.classList.add   ('inactive'); });
+global.addEventListener('focus', () => { document.body.classList.remove('inactive'); });
 
 render((
     <div id="app">
