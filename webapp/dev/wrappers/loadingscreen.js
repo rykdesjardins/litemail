778f5938a2b90efa57feb24b0d765ee94d066e5f
Erik Desjardins <erik.desjardins@gmail.com>
2018-01-01 00:14:43 -0500

    NEW YEAR COMMIT

--- webapp/dev/wrappers/loadingscreen.js
+++ webapp/dev/wrappers/loadingscreen.js
@@ -8,7 +8,11 @@ export default class LoadingScreen extends Component {
     render() {
         return (
             <div id="loading-screen">
-                <img src="assets/envelop.png"  />
+                <img src="assets/envelop.png" id="big-envelop"  />
+                <form id="initloginform">
+                    <input placeholder="Email" type="text" id="initemail" />
+                    <input placeholder="Password" type="password" id="initpass" />
+                </form>
             </div>
         );
     }
