a6b2e3c19530ff2eee06b7f7dbddce3e3fdc248b
Erik Desjardins <erik.desjardins@gmail.com>
2018-01-06 22:30:54 -0500

    Login screen will send auth request

--- webapp/dev/wrappers/loadingscreen.js
+++ webapp/dev/wrappers/loadingscreen.js
@@ -1,8 +1,55 @@
 import { h, Component } from 'preact';
+import Spinner from '../reusable/spinner';
+import { Authenticate } from '../lib/backend';
+
+const VALID_STATE = {
+    PENDING : 0,
+    LOGGEDIN : 1,
+    INVALID : 2
+};
 
 export default class LoadingScreen extends Component {
     constructor() {
         super();
+
+        this.values = {
+            initemail : "",
+            initpass : ""
+        }
+
+        this.state.activeForm = true;
+        this.state.spinner = false;
+        this.state.validState = VALID_STATE.PENDING;
+    }
+
+    sendInitAuthCombo() {
+        Authenticate(this.values.initemail, this.values.initpass).then(() => {
+            this.setState(Object.assign(this.state, { validState : VALID_STATE.LOGGEDIN, spinner : false }));
+        }, () => {
+            this.setState(Object.assign(this.state, { validState : VALID_STATE.INVALID, spinner : false }));
+        });
+    }
+
+    hideForm(done) {
+        this.setState(Object.assign(this.state, { activeForm : false }));
+        setTimeout(() => {
+            this.setState(Object.assign(this.state, { spinner : true }));
+            done && done();
+        }, 200);
+    }
+
+    validForm() {
+        return this.values.initemail.trim() && this.values.initpass.trim();
+    }
+
+    inputKeyDown(ev) {
+        this.values[ev.target.id] = ev.target.value;
+
+        if (ev.keyCode == 13 && this.validForm()) {
+            this.hideForm(() => {
+                this.sendInitAuthCombo();
+            });
+        }
     }
 
     render() {
@@ -10,8 +57,15 @@ export default class LoadingScreen extends Component {
             <div id="loading-screen">
                 <img src="assets/envelop.png" id="big-envelop"  />
                 <form id="initloginform">
-                    <input placeholder="Email" type="text" id="initemail" />
-                    <input placeholder="Password" type="password" id="initpass" />
+                    {
+                        this.state.spinner ? null : (
+                            <div>
+                                <input onKeyDown={this.inputKeyDown.bind(this)} className={this.state.activeForm ? "visible" : "invisible"} placeholder="Email" type="text" id="initemail" />
+                                <input onKeyDown={this.inputKeyDown.bind(this)} className={this.state.activeForm ? "visible" : "invisible"} placeholder="Password" type="password" id="initpass" />
+                            </div>
+                        )
+                    }
+                    <Spinner visible={this.state.spinner} />
                 </form>
             </div>
         );
